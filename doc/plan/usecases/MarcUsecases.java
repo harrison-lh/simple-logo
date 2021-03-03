20b9f47a96d68f89186a31a6f7a23370f5ee2b27
Marc Chmielewski <msc68@duke.edu>
2021-03-02 22:44:19 -0500

    Dummy Turtle class - DO NOT USE THIS IN PRODUCTION!

--- doc/plan/usecases/MarcUsecases.java
+++ doc/plan/usecases/MarcUsecases.java
@@ -12,8 +12,67 @@ private class MarcUsecases {
 
     Turtle turtle = new Turtle(); // Default constructor will have penDown, and isVisible by default
     for(Command command : commandList) {
-      turtle.executeCommand(command); // Execution of command is handled by the Turtle
+      try {
+        int successful = 0;
+        if(turtle.hasCommand(command))
+          successful = turtle.executeCommand(command); // Execution of command is handled by the Turtle
+        if(successful == 0) {
+          throw new Exception;
+        }
+      }
     }
     turtle.alertListener(); // Wake up the listener (is this necessary?) and have it update the view
+  }
+
+  /**
+   * The user defines a new function and sees the function appear in the function view
+   */
+  public void declareFunction(String commandString) {
+    CommandParser parser = new CommandParser();
+
+    // This will contain a single command, that defines a new function.
+    List<Command> commandList = parser.parseCommnands(commandString);
+    Turtle turtle = new Turtle(); // Default constructor will have penDown, and isVisible by default
+    for(Command command : commandList) {
+      try {
+        int successful = 0;
+        if(turtle.hasCommand(command))
+          successful = turtle.executeCommand(command); // Execution of command is handled by the Turtle
+        if(successful == 0) {
+          throw new Exception;
+        }
+      }
+    }
+    turtle.alertListener(); // Wake up the listener (is this necessary?) and have it update the view
+  }
+}
+
+/**
+ * Dummy (and VERY drafty) implementation of the Turtle class with the requisite methods for these
+ * user stories.
+ */
+public class Turtle {
+  private List<Command> knownCommands;
+  private List<Variable> knownVariables;
+  public int executeCommand(Command command) {
+    if(command.getType() == FORWARDS) {
+      turtle.moveForwards(command.getMagnitude());
+    }
+    else if(command.getType() == TO) {
+      List<Token> tokenList = command.getTokens();
+      Command newCommand = new Command();
+      CommandType newCommandType = new CommandType(tokenList.get(0));
+      List<Variable> newCommandParameters = new ArrayList<>();
+      List<Command> newCommandCommands = new ArrayList<>();
+      for(Token token : tokenList) {
+        if (token.getType() == PARAM) {
+          newCommandParameters.add(new Variable(token));
+        }
+        else if (token.getType() == COMMAND) {
+          newCommandParameters.add(command);
+        }
+      }
+      knownCommands.addCommand(new Command(newCommandType, newCommandParameters, newCommandCommands));
+    }
   }
 }
