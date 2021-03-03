49a434c1b7d0484e2dd597292b2a19f89bb025f9
Marc Chmielewski <msc68@duke.edu>
2021-03-02 20:06:36 -0500

    Wrote coordinates interface

---
+++ doc/plan/api/Coordinates.java
@@ -0,0 +1,52 @@
+/**
+ * An interface that defines the properties of an object that has Coordinates.
+ *
+ * Coordinates allow the View to determine where a Turtle or other Model object is in space,
+ * as well as its heading and vice versa.
+ *
+ * @author Marc Chmielewski
+ */
+public interface Coordinates {
+
+  /**
+   * Gets the x-coordinate of the object.
+   *
+   * @return The current x-coordinate of the object
+   */
+  public double getX();
+
+  /**
+   * Sets the x-coordinate of the object.
+   *
+   * @param x The new x-coordinate of the object.
+   */
+  public void setX(double x);
+
+  /**
+   * Gets the y-coordinate of the object.
+   *
+   * @return The current y-coordinate of the object
+   */
+  public double getY();
+
+  /**
+   * Sets the y-coordinate of the object.
+   *
+   * @param y The new y-coordinate of the object
+   */
+  public void setY(double y);
+
+  /**
+   * Gets the current heading of the object in degrees.
+   *
+   * @return  heading The current heading of the object in degrees.
+   */
+  public double getHeading();
+
+  /**
+   * Sets the heading of the object in degrees.
+   *
+   * @param heading The new heading of the object in degrees.
+   */
+  public void setHeading(double heading);
+}
