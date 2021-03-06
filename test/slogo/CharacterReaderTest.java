61292fc64a0b67a7fd6398f251a77c32a0cd9ad7
Marc Chmielewski <msc68@duke.edu>
2021-03-06 15:19:53 -0500

    Test consumeSingleCharacter

--- test/slogo/CharacterReaderTest.java
+++ test/slogo/CharacterReaderTest.java
@@ -7,12 +7,13 @@ import slogo.controller.CharacterReader;
 
 public class CharacterReaderTest {
 
-  private static final String HELLO_SLOGO = "Hello, SLogo!";
+  private static final String HELLO_SLOGO = "Hello, SLogo!!";
+  private static final String HELLO_SLOGO_ODD = "Hello, SLogo!";
 
   @Test
   public void testPeekSingleCharacterSimpleString() {
-
     CharacterReader reader = new CharacterReader(HELLO_SLOGO);
+
     for (int i = 0; i < HELLO_SLOGO.length(); i++) {
       // Multiple calls to peek should return the same character
       assertEquals(reader.peekSingleCharacter(), HELLO_SLOGO.substring(0, 1));
@@ -21,7 +22,11 @@ public class CharacterReaderTest {
 
   @Test
   public void testConsumeSingleCharacterSimpleString() {
-
+    CharacterReader reader = new CharacterReader(HELLO_SLOGO);
+    for (int i = 0; i < HELLO_SLOGO.length(); i++) {
+      // Multiple calls to peek should return the same character
+      assertEquals(reader.consumeSingleCharacter(), HELLO_SLOGO.substring(i, i + 1));
+    }
   }
 
   @Test
