70b8cdf2bf13f85a7f69cc462705478a011bc068
Marc Chmielewski <msc68@duke.edu>
2021-03-06 18:07:59 -0500

    Basic Lexer skeleton

---
+++ src/slogo/controller/Lexer.java
@@ -0,0 +1,13 @@
+package slogo.controller;
+
+/**
+ * Lexer implements the tokenization behavior of the Parser. That is, the Lexer is responsible for
+ * breaking up the input stream in Tokens, currently just separated by whitespace, sanitizing for
+ * comments and whitespace, validating for legitimacy and then returning that output to the Parser
+ * for interpretation.
+ *
+ * @author Marc Chmielewski
+ */
+public class Lexer {
+
+}
