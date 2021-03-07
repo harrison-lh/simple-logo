399d22205aa145821b7b56e8beee1b956a8a95ca
David Li <david.li@duke.edu>
2021-03-06 22:18:16 -0500

    Added input button

--- src/slogo/view/InputBox.java
+++ src/slogo/view/InputBox.java
@@ -1,11 +1,23 @@
 package slogo.view;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 
-public class InputBox extends TextArea {
+public class InputBox extends BorderPane {
+
+  private TextArea myInputBoxArea;
+  private Button myInputButton;
 
   public InputBox() {
     this.setId("InputBox");
     this.getStyleClass().add("box");
+    myInputBoxArea = new TextArea();
+    myInputBoxArea.setId("InputBoxArea");
+    myInputButton = new Button("Go");
+    myInputButton.setId("InputButton");
+    myInputButton.prefWidthProperty().bind(this.widthProperty());
+    this.setTop(myInputBoxArea);
+    this.setBottom(myInputButton);
   }
 }
