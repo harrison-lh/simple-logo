0a5553c83b6cad344692c15d6155b10f4d33e91b
David Li <david.li@duke.edu>
2021-03-08 01:13:33 -0500

    Resized input box

--- src/slogo/view/InputBox.java
+++ src/slogo/view/InputBox.java
@@ -6,6 +6,8 @@ import javafx.scene.layout.BorderPane;
 
 public class InputBox extends BorderPane {
 
+  public static final int BOTTOM_HEIGHT = 180;
+
   private final TextArea myInputBoxArea;
   private final Button myInputButton;
 
@@ -14,6 +16,8 @@ public class InputBox extends BorderPane {
     this.getStyleClass().add("box");
     myInputBoxArea = new TextArea();
     myInputBoxArea.setId("InputBoxArea");
+    myInputBoxArea.setPrefHeight(BOTTOM_HEIGHT);
+
     myInputButton = new Button("Go");
     myInputButton.setId("InputButton");
     myInputButton.prefWidthProperty().bind(this.widthProperty());
