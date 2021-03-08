631ae9a4a67d9a47f0b09a1da8c0cf8aa7d57a6b
Marc Chmielewski <msc68@duke.edu>
2021-03-08 14:45:18 -0500

    Initial abstract Node

---
+++ src/slogo/controller/Node.java
@@ -0,0 +1,26 @@
+package slogo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Node represents the most basic unit of a parse-tree/AST. It is extended by several other classes,
+ * most notably Command, and allows the program to leverage polymorphism to construct trees in an
+ * efficient manner.
+ *
+ * @author Marc Chmielewski
+ * @author Harrison Huang
+ */
+public abstract class Node {
+  List<Node> children = new ArrayList<>();
+
+  /**
+   * Add an additional child Node to the Node.
+   *
+   * @param child The Node to add to the children
+   */
+  public void addChild(Node child) {
+    children.add(child);
+  }
+
+}
