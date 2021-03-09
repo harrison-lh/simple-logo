c32887be493f24dd89da47fa30d93735ca146676
Cole Spector <cgs26@duke.edu>
2021-03-09 14:32:08 -0500

    added sin command

---
+++ src/slogo/controller/commands/mathoperations/SinCommand.java
@@ -0,0 +1,23 @@
+package slogo.controller.commands.mathoperations;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import slogo.controller.Command;
+import slogo.model.Turtle;
+
+public class SinCommand extends Command {
+
+  private static final int NUM_PARAMS = 1;
+
+  public SinCommand(){
+    numParams = NUM_PARAMS;
+  }
+
+  @Override
+  public double execute(Turtle turtle) {
+    double degrees = getChildren().get(0).execute(turtle);
+
+    return Math.sin(degrees);
+  }
+}
+
