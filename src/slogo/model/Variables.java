454ea82627e78066a8b19e997ef25785de2b3b17
Cole Spector <cgs26@duke.edu>
2021-03-09 11:21:23 -0500

    created variables class

---
+++ src/slogo/model/Variables.java
@@ -0,0 +1,17 @@
+package slogo.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Variables<E>{
+
+  private Map<String, E> varMap;
+
+  public Variables(){
+    varMap = new HashMap<>();
+  }
+
+
+
+}
