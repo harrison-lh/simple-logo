8d61eec3302fbac4e049254e9df0805a92a94ca1
Cole Spector <cgs26@duke.edu>
2021-03-09 11:32:13 -0500

    filled in gridCoordinates

--- src/slogo/model/GridCoordinates.java
+++ src/slogo/model/GridCoordinates.java
@@ -2,33 +2,41 @@ package slogo.model;
 
 public class GridCoordinates implements Coordinates {
 
+  private double xPos, yPos, heading;
+
+  public GridCoordinates(double x, double y, double heading){
+    this.xPos = x;
+    this.yPos = y;
+    this.heading = heading;
+  }
+
   @Override
   public double getX() {
-    return 0;
+    return xPos;
   }
 
   @Override
   public void setX(double x) {
-
+    xPos = x;
   }
 
   @Override
   public double getY() {
-    return 0;
+    return yPos;
   }
 
   @Override
   public void setY(double y) {
-
+    yPos = y;
   }
 
   @Override
   public double getHeading() {
-    return 0;
+    return heading;
   }
 
   @Override
   public void setHeading(double heading) {
-
+    this.heading = heading;
   }
 }
