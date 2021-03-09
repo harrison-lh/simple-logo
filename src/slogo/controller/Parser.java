a5e810774780092cab5fecddd0c588b09d14f563
Marc Chmielewski <msc68@duke.edu>
2021-03-08 21:41:28 -0500

    More tokenization and parser boilerplate

--- src/slogo/controller/Parser.java
+++ src/slogo/controller/Parser.java
@@ -2,7 +2,9 @@ package slogo.controller;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Parser is the meat-and-potatoes of the SLogo Control layer. This class takes in a String of SLogo
@@ -17,29 +19,43 @@ public class Parser {
   private Lexer lexer;
   private Node root;
   private List<String> splitText;
-  private List<Token> tokenizedText;
+  private Queue<Token> tokenizedText;
 
   public Parser(TurtleController controller, String syntaxLang) {
     this.controller = controller;
     this.lexer = new Lexer(syntaxLang);
     this.root = null;
     this.splitText = new ArrayList<>();
-    this.tokenizedText = new ArrayList<>();
+    this.tokenizedText = new LinkedList<>();
   }
 
   private void splitText(String text) {
     splitText = new ArrayList<>(Arrays.asList(text.split("\\s+")));
   }
 
-  private void tokenizeText(String text) {
-    List<Token> tokenList = new ArrayList<>();
+  private void tokenizeText() {
+    Queue<Token> tokenList = new LinkedList<>();
     for(String curString : splitText) {
       tokenList.add(lexer.tokenize(curString));
     }
-    tokenizedText = tokenList;
+    this.tokenizedText = tokenList;
+  }
+
+  private void mapTokensToNodes() {
+    for(Token curToken : tokenizedText) {
+      switch(curToken) {
+        // TODO: Implement this stuff
+      }
+    }
   }
 
   public void setSyntaxLang(String syntaxLang) {
     lexer.setLangSymbols(syntaxLang);
+  }
+
+  public void createParseTree(String text) {
+    splitText(text);
+    tokenizeText();
+    mapTokensToNodes();
   }
 }
