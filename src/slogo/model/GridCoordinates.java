626686529c767a56856e6d3dcc435927326fc37c
Cole Spector <cgs26@duke.edu>
2021-03-09 11:29:03 -0500

    created GridCoordinates class

---
+++ src/slogo/model/GridCoordinates.java
@@ -0,0 +1,34 @@
+package slogo.model;
+
+public class GridCoordinates implements Coordinates {
+
+  @Override
+  public double getX() {
+    return 0;
+  }
+
+  @Override
+  public void setX(double x) {
+
+  }
+
+  @Override
+  public double getY() {
+    return 0;
+  }
+
+  @Override
+  public void setY(double y) {
+
+  }
+
+  @Override
+  public double getHeading() {
+    return 0;
+  }
+
+  @Override
+  public void setHeading(double heading) {
+
+  }
+}
