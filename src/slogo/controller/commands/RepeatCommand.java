5eae47fac9791156792009bf1a53f22b482dd899
Marc Chmielewski <msc68@duke.edu>
2021-03-10 20:49:05 -0500

    RepeatCommand docs

--- src/slogo/controller/commands/RepeatCommand.java
+++ src/slogo/controller/commands/RepeatCommand.java
@@ -19,12 +19,19 @@ public class RepeatCommand extends Command {
     setNumParams(NUM_PARAMS);
   }
 
+  /**
+   * Implementation of the RepeatCommand execute method. Forces the children of the listNode to
+   * repeat as many times as the value of parameter 0. Returns the value of the last operation.
+   *
+   * @param turtle The turtle on which the Commands are acting
+   * @return Returns the return value of the last operation.
+   */
   @Override
   public double execute(Turtle turtle) {
-    assert(getChildren().size() == getNumParams());
+    assert (getChildren().size() == getNumParams());
 
     double lastVal = 0.0;
-    for(int i = 0; i < getChildren().get(0).execute(turtle); i++) {
+    for (int i = 0; i < getChildren().get(0).execute(turtle); i++) {
       lastVal = getChildren().get(1).execute(turtle);
     }
     return lastVal;
