b1a6876658758484b64444af5dae641448f71803
Cole Spector <cgs26@duke.edu>
2021-03-12 16:17:34 -0500

    created file for if command

---
+++ src/slogo/controller/commands/IfCommand.java
@@ -0,0 +1,12 @@
+package slogo.controller.commands;
+
+import slogo.controller.Command;
+import slogo.model.Turtle;
+
+public class IfCommand extends Command {
+
+  @Override
+  protected double executeCommand(Turtle turtle) {
+    return 0;
+  }
+}
