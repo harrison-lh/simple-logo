490add0a514fddf70da5dce22fdb25e637eabe26
Cole Spector <cgs26@duke.edu>
2021-03-12 16:25:06 -0500

    added two positive, one negative if tests

--- test/slogo/ParserTests.java
+++ test/slogo/ParserTests.java
@@ -505,4 +505,31 @@ public class ParserTests {
     controller.runCommands();
     assertEquals(initY + 125, turtle.getY());
   }
+
+  @Test
+  public void testIfCommand() {
+    double initY = turtle.getY();
+    parser.parseCommandString("IF 10 [ fd 50 ]");
+    controller.setIsAllowedToExecute(true);
+    controller.runCommands();
+    assertEquals(initY + 50, turtle.getY());
+  }
+
+  @Test
+  public void testComplexIfCommand() {
+    double initY = turtle.getY();
+    parser.parseCommandString("IF EQUAL? 10 10 [ fd 50 ]");
+    controller.setIsAllowedToExecute(true);
+    controller.runCommands();
+    assertEquals(initY + 50, turtle.getY());
+  }
+
+  @Test
+  public void testNegativeComplexIfCommand() {
+    double initY = turtle.getY();
+    parser.parseCommandString("IF EQUAL? 5 10 [ fd 50 ]");
+    controller.setIsAllowedToExecute(true);
+    controller.runCommands();
+    assertEquals(initY, turtle.getY());
+  }
 }
