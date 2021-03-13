584762c4d35caab145a64f1450e28057c2ca3a03
Harrison Huang <harrison.huang@duke.edu>
2021-03-12 22:48:10 -0500

    home command resets the heading

--- src/slogo/controller/commands/HomeCommand.java
+++ src/slogo/controller/commands/HomeCommand.java
@@ -5,7 +5,7 @@ import slogo.controller.ConstantCommand;
 import slogo.model.Turtle;
 
 /**
- * Home is a type of Command that places the Turtle at (0,0).
+ * Home is a type of Command that places the Turtle at (0,0) and resets the heading to 90 degrees.
  *
  * @author Harrison Huang
  */
@@ -22,13 +22,17 @@ public class HomeCommand extends Command {
   }
 
   /**
-   * Sets the position of the turtle to be at home (0,0).
+   * Sets the position of the turtle to be at home (0,0) and the heading to be 90 degrees.
    *
    * @param turtle The turtle to be set
    * @return The distance the turtle moved
    */
   @Override
   protected double executeCommand(Turtle turtle) {
+    Command setHeading = new SetHeadingCommand();
+    setHeading.addChild(new ConstantCommand(90));
+    setHeading.execute(turtle);
+
     Command setXY = new SetPositionCommand();
     setXY.addChild(new ConstantCommand(0));
     setXY.addChild(new ConstantCommand(0));
