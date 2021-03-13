b5721d7984035cd6cca153dc2c7bb5bd37f88e4d
David Li <david.li@duke.edu>
2021-03-12 22:36:50 -0500

    Implemented propertyChange in UDCommandsBox

--- src/slogo/view/UDCommandsBox.java
+++ src/slogo/view/UDCommandsBox.java
@@ -1,27 +1,56 @@
 package slogo.view;
 
-import javafx.scene.Group;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 /**
- * Display box of user defined commands
+ * Display box of user defined commands For now, only displays the command name
  *
  * @author David Li
  */
-public class UDCommandsBox extends ScrollPane {
+public class UDCommandsBox extends ScrollPane implements PropertyChangeListener {
 
-  private Group myContents;
+  private final VBox myContents;
+  private final List<String> myUDCommandsNamesList;
+  private final List<Label> myUDCommandsTextList;
 
   /**
    * Main constructor
    */
   public UDCommandsBox() {
     this.setId("UDCommandsBox");
-    myContents = new Group();
+    myContents = new VBox();
     this.setContent(myContents);
     Label title = new Label("User-Defined Commands");
     title.getStyleClass().add("box-title");
     myContents.getChildren().add(title);
+    myUDCommandsNamesList = new ArrayList<>();
+    myUDCommandsTextList = new ArrayList<>();
+  }
+
+  @Override
+  public void propertyChange(PropertyChangeEvent evt) {
+    String command = (String) evt.getNewValue();
+    if (evt.getPropertyName().equals("ADD")) {
+      addCommand(command);
+    } else if (evt.getPropertyName().equals("UPDATE")) {
+      updateCommand(command);
+    }
+  }
+
+  private void updateCommand(String command) {
+    myUDCommandsTextList.get(myUDCommandsNamesList.indexOf(command)).setText(command);
+  }
+
+  private void addCommand(String command) {
+    myUDCommandsNamesList.add(command);
+    Label commandText = new Label(command);
+    myUDCommandsTextList.add(commandText);
+    myContents.getChildren().add(commandText);
   }
 }
