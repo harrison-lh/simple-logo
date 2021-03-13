c55cada74f3ca249125053dc0fb4dc29bcdb56c5
David Li <david.li@duke.edu>
2021-03-13 00:31:15 -0500

    Refactored main class

--- src/slogo/Main.java
+++ src/slogo/Main.java
@@ -8,40 +8,37 @@ import slogo.controller.Controller;
 import slogo.view.MainView;
 
 /**
- * Starting point of the application, creates the Controller and
- * sets up the MainView in the JavaFX stage
+ * Starting point of the application, creates the Controller and sets up the MainView in the JavaFX
+ * stage
  *
  * @author David Li
  */
 public class Main extends Application {
 
-    public static final int WIDTH = 960;
-    public static final int HEIGHT = 720;
-    public static final int MIN_WIDTH = 960;
-    public static final int MIN_HEIGHT = 720;
+  public static final int MIN_WIDTH = 960;
+  public static final int MIN_HEIGHT = 720;
 
-    /**
-     * Start of the program.
-     */
-    public static void main (String[] args) {
-        launch(args);
-    }
+  /**
+   * Start of the program.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-    /**
-     * Create Controller object, and assigns the MainView to the
-     * scene
-     */
-    @Override
-    public void start(Stage stage) {
-        Controller controller = new Controller();
-        MainView mainView = controller.getMainView();
-        Scene scene = new Scene(mainView, WIDTH, HEIGHT);
-        scene.getStylesheets().add("slogo/view/stylesheet.css");
-        stage.setScene(scene);
-        stage.setMinWidth(MIN_WIDTH);
-        stage.setMinHeight(MIN_HEIGHT);
-        stage.widthProperty().addListener((obs, oldVal, newVal) -> mainView.resizeElements());
-        stage.heightProperty().addListener((obs, oldVal, newVal) -> mainView.resizeElements());
-        stage.show();
-    }
+  /**
+   * Create Controller object, and assigns the MainView to the scene
+   */
+  @Override
+  public void start(Stage stage) {
+    Controller controller = new Controller();
+    MainView mainView = controller.getMainView();
+    Scene scene = new Scene(mainView, MIN_WIDTH, MIN_HEIGHT);
+    scene.getStylesheets().add("slogo/view/stylesheet.css");
+    stage.setScene(scene);
+    stage.setMinWidth(MIN_WIDTH);
+    stage.setMinHeight(MIN_HEIGHT);
+    stage.widthProperty().addListener((obs, oldVal, newVal) -> mainView.resizeElements());
+    stage.heightProperty().addListener((obs, oldVal, newVal) -> mainView.resizeElements());
+    stage.show();
+  }
 }
