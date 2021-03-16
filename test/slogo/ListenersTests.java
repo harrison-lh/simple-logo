e12559cb83cdc2abd499f2af6350444a055de127
David Li <david.li@duke.edu>
2021-03-16 01:03:46 -0400

    Updated test classes

--- test/slogo/ListenersTests.java
+++ test/slogo/ListenersTests.java
@@ -14,25 +14,19 @@ import util.DukeApplicationTest;
 
 class ListenersTests extends DukeApplicationTest {
 
-  public static final int WIDTH = 960;
-  public static final int HEIGHT = 720;
-  public static final int MIN_WIDTH = 960;
-  public static final int MIN_HEIGHT = 720;
-
   private Turtle myTurtle;
   private Parser myParser;
 
   @Override
   public void start(Stage stage) {
     Controller controller = new Controller();
-    myParser = controller.getParser();
-    myTurtle = controller.getTurtle();
     MainView mainView = controller.getMainView();
-    Scene scene = new Scene(mainView, WIDTH, HEIGHT);
+    myTurtle = controller.getTurtle();
+    Scene scene = new Scene(mainView, Main.MIN_WIDTH, Main.MIN_HEIGHT);
     scene.getStylesheets().add("slogo/view/stylesheet.css");
     stage.setScene(scene);
-    stage.setMinWidth(MIN_WIDTH);
-    stage.setMinHeight(MIN_HEIGHT);
+    stage.setMinWidth(Main.MIN_WIDTH);
+    stage.setMinHeight(Main.MIN_HEIGHT);
     stage.show();
   }
 
--- test/slogo/ViewTests.java
+++ test/slogo/ViewTests.java
@@ -27,20 +27,15 @@ import util.DukeApplicationTest;
 
 class ViewTests extends DukeApplicationTest {
 
-  public static final int WIDTH = 960;
-  public static final int HEIGHT = 720;
-  public static final int MIN_WIDTH = 960;
-  public static final int MIN_HEIGHT = 720;
-
   @Override
   public void start(Stage stage) {
     Controller controller = new Controller();
     MainView mainView = controller.getMainView();
-    Scene scene = new Scene(mainView, WIDTH, HEIGHT);
+    Scene scene = new Scene(mainView, Main.MIN_WIDTH, Main.MIN_HEIGHT);
     scene.getStylesheets().add("slogo/view/stylesheet.css");
     stage.setScene(scene);
-    stage.setMinWidth(MIN_WIDTH);
-    stage.setMinHeight(MIN_HEIGHT);
+    stage.setMinWidth(Main.MIN_WIDTH);
+    stage.setMinHeight(Main.MIN_HEIGHT);
     stage.show();
   }
 
