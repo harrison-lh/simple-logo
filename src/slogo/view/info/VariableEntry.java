b86ccd2215b920b58c391463dd063b76ec5498d1
David Li <david.li@duke.edu>
2021-03-17 23:08:03 -0400

    Can set variable from variablesbox, throws error when invalid input

--- src/slogo/view/info/VariableEntry.java
+++ src/slogo/view/info/VariableEntry.java
@@ -1,6 +1,9 @@
 package slogo.view.info;
 
 import java.util.function.Consumer;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextInputDialog;
 import slogo.view.ClickableEntry;
 
 public class VariableEntry extends ClickableEntry<String> {
@@ -21,7 +24,23 @@ public class VariableEntry extends ClickableEntry<String> {
 
   @Override
   protected void onClick(Consumer<String> consumer) {
-    System.out.println("set variable");
+    TextInputDialog inputDialog = new TextInputDialog();
+    inputDialog.setHeaderText(null);
+    inputDialog.setGraphic(null);
+    inputDialog.setContentText(myName.substring(1) + " =");
+    inputDialog.showAndWait().ifPresent(result -> {
+      try {
+        double newValue = Double.parseDouble(result);
+        consumer.accept(setVariableCommand(newValue));
+      } catch(NumberFormatException exception) {
+        Alert alert = new Alert(AlertType.ERROR, "Invalid input");
+        alert.showAndWait();
+      }
+    });
+  }
+
+  private String setVariableCommand(double newValue) {
+    return "make " + myName + " " + newValue;
   }
 
   private String displayText() {
