f9d3b93a739e4bf6801d0cb6ab1d67adebca0a78
Marc Chmielewski <msc68@duke.edu>
2021-03-20 14:15:29 -0400

    Implemented loadVariables

--- src/util/LibraryManager.java
+++ src/util/LibraryManager.java
@@ -1,5 +1,6 @@
 package util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
@@ -9,19 +10,58 @@ import java.util.Map;
  * LibraryManager is a utility class that allows the program to save the variables and user-defined
  * functions to JSON files, which can then be loaded back into the program at a later time.
  *
+ * Storing information is handled by passing in a Map that takes the general form
+ * Map<NAME_OF_THING_TO_STORE, THING_TO_STORE> while loading handled by reading the file in, and
+ * converting into a SLogo command, which is then passed to the Parser and run.
+ *
  * @author Marc Chmielewski
  */
 public class LibraryManager {
 
-  public static void saveLibrary(String filename, Map<String, Double> varMap,
-      Map<String, String> userCommandMap)
+  private static final ObjectMapper mapper = new ObjectMapper();
+
+  /**
+   * saveVariables saves the contents of varMap to a JSON file, which can then be loaded back in
+   * at a later time.
+   *
+   * @param filename The filename of the JSON file.
+   * @param varMap The map of Strings and Doubles that represents the variables.
+   * @throws IOException On invalid filenames and paths
+   */
+  public static void saveVariables(String filename, Map<String, Double> varMap)
       throws IOException {
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.writeValue(new File("resources/libraries/variables/" + filename + "_vars.json"), varMap);
-    mapper.writeValue(new File("resources/libraries/user-commands/" + filename + "_ucs.json"), userCommandMap);
+    mapper.writeValue(new File("resources/libraries/variables/" + filename), varMap);
   }
 
-  public static void loadLibrary() {
+  /**
+   * saveUserCommands saves the contents of userCommandMap to a JSON file, which can then be
+   * loaded back in at a later time.
+   *
+   * @param filename The filename of the JSON file.
+   * @param userCommandMap The map of Strings and Strings that represents the commands.
+   * @throws IOException On invalid filenames and paths
+   */
+  public static void saveUserCommands(String filename, Map<String, String> userCommandMap)
+      throws IOException {
+    mapper.writeValue(new File("resources/libraries/user-commands/" + filename),
+        userCommandMap);
+  }
+
+
+  public static String loadVariables(String filename) throws IOException {
+    Map<String, Double> varMap
+        = mapper.readValue(new File("resources/libraries/variables/" + filename),
+        new TypeReference<>() {});
+    StringBuilder loadVarCommand = new StringBuilder();
+    for(String curVar : varMap.keySet()) {
+      loadVarCommand.append("MAKE ").append(curVar).append(" ").append(varMap.get(curVar))
+          .append(" ");
+    }
+    return loadVarCommand.toString().trim();
+  }
+
+  public static String loadUserCommands() {
     // TODO: Implement this!
+    return null;
   }
 }
--- test/util/LibraryManagerTests.java
+++ test/util/LibraryManagerTests.java
@@ -10,26 +10,43 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class LibraryManagerTests {
+
   @Test
-  public void testSaveLibrary() {
+  public void testSaveVariables() {
     Map<String, Double> testVars = new HashMap<>();
     testVars.put(":A", 50.0);
     testVars.put(":B", 75.0);
     testVars.put(":DUKE", 2022.0);
 
-    Map<String, String> testUserCommands = new HashMap<>();
-    testUserCommands.put("square", "TO square [ ] [ repeat 4 [ fd 50 rt 90 ] ]");
     try {
-      LibraryManager.saveLibrary("test", testVars, testUserCommands);
+      LibraryManager.saveVariables("test.json", testVars);
     } catch (IOException e) {
       System.err.println(e.getMessage());
       fail("Failed to create file!");
     }
 
-    File savedVars = new File("resources/libraries/variables/test_vars.json");
+    File savedVars = new File("resources/libraries/variables/test.json");
     assertTrue(savedVars.exists());
+  }
 
-    File savedUCs = new File("resources/libraries/user-commands/test_ucs.json");
+  @Test
+  public void testSaveUserCommands() {
+    Map<String, String> testUserCommands = new HashMap<>();
+    testUserCommands.put("square", "TO square [ ] [ repeat 4 [ fd 50 rt 90 ] ]");
+
+    try {
+      LibraryManager.saveUserCommands("test.json", testUserCommands);
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+      fail("Failed to create file!");
+    }
+
+    File savedUCs = new File("resources/libraries/user-commands/test.json");
     assertTrue(savedUCs.exists());
+  }
+
+  @Test
+  public void testLoadVariables() {
+
   }
 }
