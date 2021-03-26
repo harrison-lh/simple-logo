467d88b72f49e686a7b4c5f7f617180d6600b45d
Cole Spector <cgs26@duke.edu>
2021-03-26 16:28:41 -0400

    fixed javadoc

--- src/slogo/controller/commands/ArcTangentCommand.java
+++ src/slogo/controller/commands/ArcTangentCommand.java
@@ -6,17 +6,17 @@ import slogo.model.Turtle;
 
 /**
  * This class is used when the user types the ArcTangent command into the command line, and converts the given value to its arc tangent.
- * It assumes that the user provides one "child", or subsequent command -- most likely a constant -- which in terms of degrees to get
- * the ArcTan of, and will throw an exception if this assumption isn't valid.
+ * It assumes that the user provides one "child", or subsequent command -- most likely a constant -- which are of type Command, and calling .execute on these
+ * Commands will return a double to get the ArcTan of, and will throw an exception if this assumption isn't valid.
  * This class is dependant on the Turtle, GlobalProperties, and Java.Math classes/packages in order to function.
  *
  * Example Code:
  *
- * ...
+ * ... // assume to have previously made a Turtle.java object named turtle, and a GlobalParameters.java object named globalParams
  * Command arcTanCommand = new ArcTangentCommand();
  * double degreesToConvert = 45;
  * arcTanCommand.addChild(new ConstantCommand(degreesToConvert));
- * double arcTanOfDegreesToConvert = arcTanCommand.execute;
+ * double arcTanOfDegreesToConvert = arcTanCommand.execute(turtle, globalParams);
  * ...
  *
  * A thing to note when using ArcTangentCommand.java is that when it is time to calculate the arcTan, .execute should be called
