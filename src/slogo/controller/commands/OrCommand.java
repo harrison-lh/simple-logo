315c7106bdd9cfb59ad39e75826b176c0bc5f736
Cole Spector <cgs26@duke.edu>
2021-03-27 16:01:52 -0400

    added javadoc

--- src/slogo/controller/commands/OrCommand.java
+++ src/slogo/controller/commands/OrCommand.java
@@ -5,9 +5,39 @@ import slogo.controller.GlobalProperties;
 import slogo.model.Turtle;
 
 /**
- * Or is a type of Command that checks whether at least one value is nonzero.
+ * This class is used when the user types the And command into the command line, and will check to see if both values input after
+ * are not equal to 0, in which case it will return 1.  Otherwise it will return 0.
  *
- * @author Harrison Huang
+ * It assumes that the user provides two "children", or subsequent commands, which are in the form of Command and calling .execute on these
+ * Commands will return a double.  These two children will be checked to see if they are both present, and will throw an exception if this assumption isn't upheld.
+ * This class is dependant on the Turtle and GlobalProperties classes in order to function.
+ *
+ * Example Code:
+ *
+ * ... // assume to have previously made a Turtle.java object named turtle, and a GlobalParameters.java object named globalParams
+ * Command nonZeroOrCommand = new OrCommand();
+ * Command zeroOrCommand = new OrCommand();
+ * double firstValue = 45;
+ * double zeroValue = 0;
+ * Command firstConstant = new ConstantCommand(firstValue);
+ * Command zeroConstant = new ConstantCommand(zeroValue);
+ * nonZeroOrCommand.addChild(firstConstant);
+ * nonZeroOrCommand.addChild(zeroConstant);
+ * zeroOrCommand.addChild(zeroConstant);
+ * zeroOrCommand.addChild(zeroConstant);
+ *
+ * double one = nonZeroOrCommand.execute(turtle, globalParams)
+ * //one will have a value of 1
+ * double zero = zeroOrCommand.execute(turtle, globalParams)
+ * //zero will have a value of 0
+ * ...
+ *
+ * A thing to note when using AndCommand.java is that when it is time to calculate the and-value, .execute should be called
+ * and not .executeCommand, for .execute has the check for NUM_PARAMS.
+ *
+ * @Author Harrison Huang
+ * @Author Cole Spector
+ *
  */
 
 public class OrCommand extends Command {
