a6ee73d5c13e91d3b06d3ef7a969fe47ecb159cf
Cole Spector <cgs26@duke.edu>
2021-03-27 15:59:11 -0400

    added javadoc

--- src/slogo/controller/commands/NotEqualCommand.java
+++ src/slogo/controller/commands/NotEqualCommand.java
@@ -5,10 +5,37 @@ import slogo.controller.GlobalProperties;
 import slogo.model.Turtle;
 
 /**
- * NotEqual is a type of Command that compares two values and checks whether the first value is not
- * equal to the second value.
+ * This class is used when the user types the NotEqual command into the command line, and compares the two values given.
+ * It assumes that the user provides two "children", or subsequent commands -- most likely a constant -- which are of type Command, and calling .execute on these
+ * Commands will return a double, and will throw an exception if this assumption isn't valid.
+ * This class is dependant on the Turtle, GlobalProperties.
  *
- * @author Harrison Huang
+ * Example Code:
+ *
+ * ... // assume to have previously made a Turtle.java object named turtle, and a GlobalParameters.java object named globalParams
+ * Command equalCommand = new NotEqualCommand();
+ * Command notEqualCommand = new NotEqualCommand();
+ *
+ * double firstVal = 50;
+ * Command firstConstant = new ConstantCommand(firstVal);
+ * equalCommand.addChild(firstConstant);
+ * notEqualCommand.addChild(firstConstant);
+ * double secondVal = 50;
+ * Command secondConstant = new ConstantCommand(secondVal);
+ * equalCommand.addChild(secondConstant);
+ * double thirdVal = 45;
+ * Command thirdConstant = new ConstantCommand(thirdVal);
+ * notEqualCommand.addChild(thirdConstant);
+ *
+ * double isEqual = equalCommand.execute(turtle, globalParams); // this will be 0 in this example
+ * double notEqual = notEqual.execute(turtle, globalParams); // this will be 1 in this example
+ * ...
+ *
+ * A thing to note when using NotEqualCommand.java is that when it is time to compare, .execute should be called
+ * and not .executeCommand, for .execute has the check for NUM_PARAMS.
+ *
+ * @Author Cole Spector
+ *
  */
 
 public class NotEqualCommand extends Command {
