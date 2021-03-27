f7cda61cc8516a22c09b3e882f902c0a60aadee9
Cole Spector <cgs26@duke.edu>
2021-03-27 15:57:18 -0400

    added javadoc

--- src/slogo/controller/commands/NotCommand.java
+++ src/slogo/controller/commands/NotCommand.java
@@ -5,9 +5,34 @@ import slogo.controller.GlobalProperties;
 import slogo.model.Turtle;
 
 /**
- * Not is a type of Command that returns 1 if the value is 0, and 0 if the value is nonzero.
+ * This class is used when the user types the Not command into the command line, checks to see if the value given is 0.
+ * It assumes that the user provides one "child", or subsequent command -- most likely a constant -- which are of type Command, and calling .execute on these
+ * Commands will return a double, and will throw an exception if this assumption isn't valid.
+ * This class is dependant on the Turtle, GlobalProperties.
  *
- * @author Harrison Huang
+ * Example Code:
+ *
+ * ... // assume to have previously made a Turtle.java object named turtle, and a GlobalParameters.java object named globalParams
+ * Command notCommand = new NotCommand();
+ * Command yesCommand = new NotCommand();
+ *
+ * double val = 0;
+ * Command constant = new ConstantCommand(val);
+ * notCommand.addChild(constant);
+ *
+ * double zeroVal = 50;
+ * Command zeroConstant = new ConstantCommand(zeroVal);
+ * yesCommand.addChild(zeroConstant);
+ *
+ * double notVal = notCommand.execute(turtle, globalParams); // this will be 1 in this example
+ * double yesVal = yesCommand.execute(turtle, globalParams); // this will be 0 in this example
+ * ...
+ *
+ * A thing to note when using MinusCommand.java is that when it is time to calculate the negative, .execute should be called
+ * and not .executeCommand, for .execute has the check for NUM_PARAMS.
+ *
+ * @Author Cole Spector
+ *
  */
 
 public class NotCommand extends Command {
