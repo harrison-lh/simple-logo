23f93741e171d61ac55be704ed7bcec3aa0a07ae
Cole Spector <cgs26@duke.edu>
2021-03-27 15:39:22 -0400

    added some javadoc

--- src/slogo/controller/commands/GreaterThanCommand.java
+++ src/slogo/controller/commands/GreaterThanCommand.java
@@ -5,10 +5,39 @@ import slogo.controller.GlobalProperties;
 import slogo.model.Turtle;
 
 /**
- * GreaterThan is a type of Command that compares two values and checks whether the first value is
- * strictly greater than the second value.
+ * This class is used when the user types the Equal command into the command line, and compares the two values given.
+ * It assumes that the user provides two "children", or subsequent commands -- most likely a constant -- which are of type Command, and calling .execute on these
+ * Commands will return a double, and will throw an exception if this assumption isn't valid.
+ * This class is dependant on the Turtle, GlobalProperties.
  *
- * @author Harrison Huang
+ * Example Code:
+ *
+ * ... // assume to have previously made a Turtle.java object named turtle, and a GlobalParameters.java object named globalParams
+ * Command greaterCommand = new GreaterThanCommand();
+ * Command lessCommand = new GreaterThanCommand();
+ *
+ * double firstVal = 50;
+ * Command firstConstant = new ConstantCommand(firstVal);
+ * greaterCommand.addChild(firstConstant);
+ * lessCommand.addChild(firstConstant);
+ * double secondVal = 45;
+ * Command secondConstant = new ConstantCommand(secondVal);
+ *
+ * greaterCommand.addChild(firstConstant);
+ * lessCommand.addChild(secondConstant);
+ *
+ * greaterCommand.addChild(secondConstant);
+ *  * lessCommand.addChild(firstConstant);
+ *
+ * double greaterThan = greaterCommand.execute(turtle, globalParams); // this will be 1 in this example
+ * double lessThan = lessCommand.execute(turtle, globalParams); // this will be 0 in this example
+ * ...
+ *
+ * A thing to note when using GreaterThanCommand.java is that when it is time to compare, .execute should be called
+ * and not .executeCommand, for .execute has the check for NUM_PARAMS.
+ *
+ * @Author Cole Spector
+ *
  */
 
 public class GreaterThanCommand extends Command {
