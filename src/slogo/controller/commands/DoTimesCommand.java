dc4266967464fc4e91de4054acc80cf4487f4a53
Cole Spector <cgs26@duke.edu>
2021-03-27 14:50:07 -0400

    added javadoc comments

--- src/slogo/controller/commands/DoTimesCommand.java
+++ src/slogo/controller/commands/DoTimesCommand.java
@@ -6,6 +6,43 @@ import slogo.controller.ListCommandHead;
 import slogo.controller.VariableCommand;
 import slogo.model.Turtle;
 
+/**
+ * This class is used when the user types the DoTimes command into the command line, and will repeat the commands given in the second parameter,
+ * a list, as many times as defined in the first parameter, also a list, and returns the value of the last run command on the last turtle.
+ *
+ * It assumes that the user provides two "children", or subsequent commands, which are in the form of a ListCommand and calling .execute on these
+ * Commands will return a double.  These two children will be checked to see if they are both present, and will throw an exception if this assumption isn't upheld.
+ * This class is dependant on the Turtle, ListCommandHead and GlobalProperties classes in order to function.
+ *
+ * Example Code:
+ *
+ * ... // assume to have previously made a Turtle.java object named turtle, and a GlobalParameters.java object named globalParams
+ * Command doTimesCommand = new DoTimesCommand();
+ *
+ * Command numList = new ListCommandHead();
+ * double repeatNum = 5;
+ * Command repeatConstant = new ConstantCommand(repeatNum);
+ * numList.addInnerChild(repeatConstant);
+ * doTimesCommand.addChild(doTimesCommand);
+ *
+ * Command commandList = new ListCommandHead();
+ * double amtToMove = 50;
+ * Command moveConstant = new ConstantCommand(amtToMove);
+ * Command moveCommand = new ForwardCommand();
+ * moveCommand.addChild(moveConstant);
+ * commandList.addInnerChild(moveCommand);
+ * doTimesCommand.addChild(commandList);
+ *
+ * doTimesCommand.execute(turtle, globalParams);
+ * // the turtle will move forward 50 pixels, 5 times.
+ * ...
+ *
+ * A thing to note when using DoTimesCommand.java is that when it is time to run, .execute should be called
+ * and not .executeCommand, for .execute has the check for NUM_PARAMS.
+ *
+ * @Author Cole Spector
+ *
+ */
 public class DoTimesCommand extends Command {
 
   private static final int NUM_PARAMS = 2;
